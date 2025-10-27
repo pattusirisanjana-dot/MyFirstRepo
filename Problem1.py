9cfb0803f7b39533f2b0841adf716f803ea3ace4
pattusirisanjana-dot <pattusirisanjana@gmail.com>
2025-10-27 14:16:54 +0530

    Create Problem1.py
    i hope you like my code !!

---
+++ Problem1.py
@@ -0,0 +1,8 @@
+# Ask the user to enter a number
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
